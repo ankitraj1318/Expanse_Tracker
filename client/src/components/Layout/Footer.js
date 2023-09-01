8a573cbed1a850f5d423c99c29380dc0efd38d83
Ankit Raj <101312894+ankitraj1318@users.noreply.github.com>
2023-09-01 17:15:40 +0530

    Add files via upload

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-3">
+      <h6 className="text-center text-white">
+        All rights reserved &copy; Drogon
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import "../styles/Loginpage.css";
+const Login = () => {
+  const img =
+    "https://st.depositphotos.com/2458127/5019/i/950/depositphotos_50196757-stock-photo-euro-currency-on-graphs-financial.jpg";
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  //from submit
+  const submitHandler = async (values) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/users/login", values);
+      setLoading(false);
+      message.success("login success");
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ ...data.user, password: "" })
+      );
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      message.error("something went wrong");
+    }
+  };
+
+  //prevent for login user
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/");
+    }
+  }, [navigate]);
+  return (
+    <>
+      <div className="login-page ">
+        {loading && <Spinner />}
+        <div className="row container">
+          <h1 className="main-heading">Expense Management</h1>
+          <div className="col-md-6">
+            <img src={img} alt="login-img" width={"100%"} height="100%" />
+          </div>
+          <div className="col-md-4 login-form">
+            <Form layout="vertical" onFinish={submitHandler}>
+              <h1>LOGIN</h1>
+
+              <Form.Item label="Email" name="email">
+                <Input type="email" required />
+              </Form.Item>
+              <Form.Item label="Password" name="password">
+                <Input type="password" required />
+              </Form.Item>
+              <div className="d-flex justify-content-between">
+                <Link to="/register">
+                SIGN UP
+                </Link>
+                <button className="btn">Login</button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
